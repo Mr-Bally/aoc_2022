eb281b66bbe4051dbb022ebf4ffc49f276aed9b4
Sam-Ballantyne <25254611+Sam-Ballantyne@users.noreply.github.com>
2023-03-24 19:34:35 +0000

    added unit tests for helper

--- src/helpers/integer_parser.rs
+++ src/helpers/integer_parser.rs
@@ -11,3 +11,23 @@ fn handle_error(error: ParseIntError, input: &str) -> i32 {
     print!("Error when parsing int {}, error {}\n", input, error);
     0
 }
+
+mod test {
+    use crate::helpers::integer_parser::parser_int_from_string;
+
+    #[test]
+    fn parser_work_for_integer() {
+        let expected = 30;
+        let actual = parser_int_from_string("30");
+        
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn parser_returns_zero_for_letters() {
+        let expected = 0;
+        let actual = parser_int_from_string("ThisWontWork");
+        
+        assert_eq!(expected, actual);
+    }
+}
